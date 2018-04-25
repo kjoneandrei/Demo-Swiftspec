b1a19b2ea3c5aa8db8a942c8dd0d91dbbaa86b33
anho <anho@nodes.dk>
2018-04-25 10:15:46 +0200

    initial swiftplate generated

---
+++ Sources/Demo.swift
@@ -0,0 +1,9 @@
+//
+//  Demo.swift
+//  Demo
+//
+//  Created by anho on 25/04/2018.
+//  Copyright © 2018 Demo. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/DemoTests/DemoTests.swift
@@ -0,0 +1,23 @@
+//
+//  DemoTests.swift
+//  Demo
+//
+//  Created by anho on 25/04/2018.
+//  Copyright © 2018 Demo. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Demo
+
+class DemoTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(Demo().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
